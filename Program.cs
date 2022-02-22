a66e3785ffddeba0dc43d96475ac40ae527ba6a7
Navdeep253 <alladanavdeep@gmail.com>
2022-02-22 16:59:53 +0530

    add this

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace ReflectionExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type t = typeof(string);
+
+            foreach(MethodInfo m in  t.GetMethods())
+            {
+              
+                Console.WriteLine(m.Name);
+            }
+        }
+    }
+}
